83d5974cba03425846d00cc4dac8224bcbc647a7
Saumya <bansalgudiya350@gmail.com>
2024-06-20 11:59:23 +0300

    updated website

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path= require("path");
 const http =require('http');
 const socketio = require('socket.io');
 const formatMessage= require('./utils/messages');
+const joinColor = ' #5cb85c';
 const {userJoin,getCurrentUser,userLeave,getRoomUsers}= require('./utils/users');
 
 const app= express();
@@ -16,16 +17,16 @@ const botName = 'chatbot';
 
 //run when client can connects
 io.on('connection',(socket)=>{
-    socket.on('joinRoom',({username,room})=>{
+    socket.on('joinRoom',({username,room})=>{      //turns on the connection in the io room 
         const user= userJoin(socket.id,username,room);
         socket.join(user.room);
 
     //Welcoming the user
-    socket.emit('message',formatMessage(botName,'welcome to Chatberry'))
+    socket.emit('message',formatMessage(botName,'welcome to Bubbletalk'))
 
     //broadcast when a user connects ...notifies everyone except the user that's connecting
 
-    socket.broadcast.to(user.room).emit('message',formatMessage(botName,`${user.username} has joined the chat`));
+    socket.broadcast.to(user.room).emit('message',formatMessage(botName,`${user.username} has joined the chat`,joinColor));
 
       //send users and room info
       io.to(user.room).emit('roomUsers',{
--- utils/messages.js
+++ utils/messages.js
@@ -1,9 +1,10 @@
 const moment = require('moment');
 
-function formatMessage(username, text){
+function formatMessage(username, text, color = '#000'){
     return{
         username,
         text,
+        color,
         time: moment().format('h:mm a')
     }
 }
